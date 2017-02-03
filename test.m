3bf154867aac740ea33238c8497a2c791b8ee118
Karol Kurek <karol.kurek@finanteq.com>
2017-02-03 11:48:08 +0100

    zmiany

--- test.m
+++ test.m
@@ -1,21 +1,24 @@
-@implementation TESTSomething
+@implementation TESTSomething <someProtocol> <anotherProtocol>
 
 - (void)setValidationMessageHidden:(BOOL)hidden {
     [_someLabel setHidden:hidden];
-    _secondSpace.priority = hidden ? 111 : 250;
+    _secondSpace,priority = hidden ? 111 : 250;
     _firstSpace.priority = hidden ? 250 : 111;
     
-    if (!hidden) {
-        [_someLabel setText:self.item.co];
+    if (!hidden == YES)
+    {
+        [_someLabel setText:    self.item.co];
     }
 }
 
 - (void)fillCurrency {
     [_currency setText:self.item.currency];
+
+
     CGRect frame = _currency.frame;
-    frame.size.width = self.item.currency != nil ? 20. : 0.;
+    frame.size.width = self.item.currency != nil ? 20. : 0;
     frame.size.height = _textField.frame.size.height;
-    [_currency setFrame:frame];
+    [_currency setFramek:frame];
 }
 
 @end
